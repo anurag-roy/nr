7e93dea4e2f3e77a80287dd691f111b675082126
Anurag Roy <anurag@2pirad.com>
2023-08-18 23:47:35 +0530

    Read `package.json` file

--- main.go
+++ main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+)
+
+type PackageJSON struct {
+	Scripts map[string]string `json:"scripts"`
+}
 
 func main() {
-	fmt.Println("Hello World")
+	// Open the package.json file
+	file, err := os.Open("package.json")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	// Read the file content into a byte slice
+	data, err := io.ReadAll(file)
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return
+	}
+
+	// Create a variable to hold the parsed JSON data
+	var packageData PackageJSON
+
+	// Unmarshal the JSON data into the PackageJSON struct
+	err = json.Unmarshal(data, &packageData)
+	if err != nil {
+		fmt.Println("Error unmarshaling JSON:", err)
+		return
+	}
+
+	// Print the extracted scripts
+	fmt.Println("Scripts:")
+	for scriptName, scriptCommand := range packageData.Scripts {
+		fmt.Printf("%s: %s\n", scriptName, scriptCommand)
+	}
 }
